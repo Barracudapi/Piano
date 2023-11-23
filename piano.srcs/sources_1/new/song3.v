f9cd6eb806f37bcdfdabc5e1f8d16929be60423d
Barracudapi <132427435+Barracudapi@users.noreply.github.com>
2023-11-23 17:19:34 +0800

    song1,2,3 + LED
    write the code for the song

---
+++ piano.srcs/sources_1/new/song3.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/11/23 17:18:03
+// Design Name: 
+// Module Name: song3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module song3(
+
+    );
+endmodule
