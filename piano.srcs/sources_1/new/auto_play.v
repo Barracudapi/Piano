f0088ff871f3dde0ecf459062ec0729a8f99cf71
Barracudapi <132427435+Barracudapi@users.noreply.github.com>
2023-11-25 16:37:00 +0800

    autoplay

--- piano.srcs/sources_1/new/auto_play.v
+++ piano.srcs/sources_1/new/auto_play.v
@@ -22,45 +22,58 @@
 
 module auto_play(
 input wire clk,
-input wire rst,
-input wire [1:0] state,
-input wire [3:0] select_songs,
+input wire reset,
+input btn, pausebtn,
 output wire melody
-    );
-    parameter  IDLE = 2'b00,
-                PLAY= 2'b01,
-                RESET= 2'b11;
-    parameter  song1 = 3'b001,
-                song2= 3'b010,
-                song3= 3'b100;        
+    ); 
     
     reg [4:0] music;
-    wire music1, music2, music3;
-    wire [11:0] frequency;
-    wire melody;
+    wire [4:0] music1;
+    wire [4:0]music2;
+    wire [4:0]music3;
+    reg [11:0] frequency;
+    reg[1:0] counter;
+    reg [1:0] state;
+    reg pause;
     
     
-    song1 s1(clk, rst, music1);
-    song2 s2(clk, rst, music2);
-    song3 s3(clk, rst, music3);
+    song1 s1(clk, reset, music1);
+    song2 s2(clk, reset, music2);
+    song3 s3(clk, reset, music3);
+    
+    music_to_frequency mf(clk, music, frequency);
+    generate_melody gm(clk, frequency, melody);
     
     
-    
-    always @(*) begin
-        case(state) 
-            PLAY: begin
-            
-            end
-            RESET:begin
-            
-            end
-            IDLE:begin
-            
-            end
-            endcase
+    always @(posedge clk, negedge reset, negedge pausebtn) begin
+        if(!reset) begin
+            counter <= 0;
+            music <= 0;
+            frequency <= 0;
+            pause <= 1'b0;
+        end else if(pausebtn) begin
+            pause <= ~pause;
         end
+        else begin
+            pause <= pause;
+        end
+        end
+       
         
-      
-          
         
+     always @(posedge clk or negedge reset) begin
+     if(!reset)
+        counter <= 0;
+     else begin
+     if(!btn)
+     counter = counter + 1;
+     if(counter == 4)
+        counter <= 0;
+     end
+     case(counter)
+        0: music = music1;
+        1: music = music2;
+        2: music = music3;
+     endcase
+     end         
 endmodule
